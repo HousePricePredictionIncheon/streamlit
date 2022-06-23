a6dbc0e0280e6d4a227417b7e0f396b01d4977f6
simjeongho <simjeongho012@gmail.com>
2022-06-23 12:34:46 +0900

    refactor: 코드 리팩토링

--- first_app.py
+++ first_app.py
@@ -2,29 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
-import openpyxl
 import xgboost as xgb
-import torch
-# wb = openpyxl.load_workbook("AllData.xlsx", read_only=False, data_only=False)
-# ws = wb['Sheet1']
-# filename = 'AllData.xlsx'
-#xgb_model = joblib.load('xgbIncheonBest_model.pkl')
+
+
 # xgb모델 불러오기
 xgb_model = xgb.XGBRegressor()
 xgb_model.load_model('xgb_load_final2.model')
-#incheondf = pd.read_excel(filename, engine='openpyxl')
 
-
+# 제목
 st.title('Incheon house Price')
 st.write('Incheon House Price Prediction Project')
-# st.write(incheondf.head())
 
-# st.write(xgb_model.predict(input2))
 
-# Add a placeholder
+# Add a placeholder 진행 상황 바
 latest_iteration = st.empty()
 bar = st.progress(0)
-
 for i in range(100):
     # Update the progress bar with each iteration.
     latest_iteration.text(f'Iteration {i+1}')
@@ -33,6 +25,8 @@ for i in range(100):
 
 # 변수는 총 11개
 # 순서대로 전용면적, 계약년월, 거래금액, 층, 건축년도, 대규모 점포, 근린공원, 반료동물 등록 수, 병원, 학교, station, starbucks
+st.write("고려한 변수는 총 11개 입니다.")
+st.write("전용면적, 계약년월, 층, 건축년도, 대규모 점포, 근린 공원, 반려동물 등록 수, 병원, 학교, 지하철 역 개수, startbucks 수")
 max_wid = 291.335999
 max_contract = 202204.0
 max_price = 450000
@@ -40,19 +34,19 @@ max_floor = 60
 max_builyear = 2022
 maxs = [291.335999, 202204.0, 60, 2022.0, 6.0, 28.0, 11863, 303, 50, 6, 5]
 mins = [11.946, 202104, 1, 1971, 0, 0, 0, 0, 0, 0, 0]
-option = st.sidebar.selectbox(
-    '어떤 지역을 고르시겠습니까?',
-    ('구월동', '용현동', '학익동', '문학동'))
-inputData = []
-with st.sidebar:
-    add_radio = st.radio(
-        "choose a parameter",
-        ("전용면적", "계약년월", "층", "건축년도")
-    )
+
+# 지역 변수를 제외한 나머지 변수 설정하는 sidebar
+
 width = st.slider("면적을 선택하세요 단위(m^2)", 11, 291)
-contract = st.slider("계약년월을 선택하세요(년월)", 202104, 202204)
+#contract = st.slider("계약년월을 선택하세요(년월)", 202104, 202204)
+# 6. 날짜 입력
+contractdate = st.date_input('계약년월')  # 디폴트로 오늘 날짜가 찍혀 있다.
+contractString = contractdate.strftime("%Y%m")
+contract = float(contractString)
+st.write(contractdate.strftime("%Y%m"))
+
 floor = st.slider("층을 선택하세요", 1, 60)
-builtYear = st.slider("건축년도를 선택하세요(년월)", 1971, 2022)
+builtYear = st.slider("건축년도를 선택하세요(년도)", 1971, 2022)
 market = 1
 park = 6
 pet = 5930
@@ -61,6 +55,28 @@ school = 16
 station = 1
 starbucks = 1
 
+# 지역을 고르는 select box
+option = st.sidebar.selectbox(
+    '어떤 지역을 고르시겠습니까?',
+    ('구월동', '용현동', '학익동', '문학동'))
+
+# scaling되기 전의 데이터
+realData = [[width, contract, floor, builtYear, market,
+             park, pet, hospital, school, station, starbucks]]
+
+# minMaxScaler를 통해 scaling해줬었으므로 여기서도 해줘야함 scaling 된 데이터를 저장할 곳
+scaleData = []
+
+# scaling
+for i in range(0, 11):
+    scaleData.append((realData[0][i] - mins[i]) / (maxs[i] - mins[i]))
+scaleData = [scaleData]
+res1 = xgb_model.predict(scaleData)
+
+# 결과 출력
+st.write(res1)
+
+# 지역에 따른 지도 보여주기 및 변수 설정
 mapalatlon = [37.4468604, 126.6551088]
 if(option == '용현동'):
     maplatlon = [37.4468604, 126.6551088]
@@ -74,21 +90,10 @@ if(option == '용현동'):
 
 elif(option == '구월동'):
     maplatlon = [37.4385657, 126.6956650]
+
+# 고른 지역 보여주기
 st.write('You selected:', option)
 map_data = pd.DataFrame(
     np.random.rand(1, 2) / [50, 50] + maplatlon,
-    #[50, 50] + maplatlon,
     columns=['lat', 'lon'])
-
 st.map(map_data)
-scaleData = []
-# 예측 과정
-realData = [[width, contract, floor, builtYear, market,
-             park, pet, hospital, school, station, starbucks]]
-input2 = [[114.860001, 202105, 4, 2001, 0, 0, 1245, 21, 15, 0, 1]]
-# scaling
-for i in range(0, 11):
-    scaleData.append((realData[0][i] - mins[i]) / (maxs[i] - mins[i]))
-scaleData = [scaleData]
-res1 = xgb_model.predict(scaleData)
-st.write(res1)
